d1770edb5d70f93a8f3a818951b7a7776d9def58
hunterkepley <foxhunter12@gmail.com>
2017-07-11 15:35:21 -0400

    Added *Gopher command

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 	"os/signal"
 	"strings"
 	"syscall"
+	"math/rand"
+	"time"
 )
 
 // Variables used for command line parameters
@@ -18,7 +20,21 @@ var (
 
 // Custom variables
 var (
-	commands []string = []string{"*Hello", "*Help", "*Gopherify"} // Commands
+	commands []string = []string{"*Hello", "*Help", "*Gopherify", "*Gopher"} // Commands
+	// Pictures of gophers:
+	gophers []string = []string{"http://i.imgur.com/3tw6sII.jpg", 
+	"http://i.imgur.com/wUoSiNI.gif", "http://i.imgur.com/NfqwhN6.gif", 
+	"http://i.imgur.com/CBvD4d5.jpg", "http://i.imgur.com/CBMlinR.jpg", 
+	"http://i.imgur.com/32uPofb.jpg", "http://i.imgur.com/8jFGjsz.jpg", 
+	"http://i.imgur.com/seTJOPL.gif", "http://i.imgur.com/pBIh3pP.gif",
+	"https://behrrake.files.wordpress.com/2008/04/fighing-gopher.jpg", 
+	"https://s-media-cache-ak0.pinimg.com/736x/63/af/78/63af782253e24944dd6d968acda29211--groundhog-pictures-happy-groundhog-day.jpg",
+	"https://s-media-cache-ak0.pinimg.com/736x/1b/5a/2f/1b5a2fa52342a3cb980dbb38282683be--family-pictures-awkward-family-photos.jpg",
+	"http://www.freakingnews.com/pictures/5500/Gopher--5651.jpg", 
+	"https://pics.me.me/i-could-spat-gopher-a-beer-funny-c3-15199885.png",
+	"http://il8.picdn.net/shutterstock/videos/7339471/thumb/1.jpg",
+	"https://s-media-cache-ak0.pinimg.com/236x/81/44/2f/81442fdb4c4e31a8e77a779cbad33b57--funny-things-funny-stuff.jpg",
+	"http://images.gr-assets.com/books/1347514988l/14478480.jpg"}
 )
 
 func init() {
@@ -27,6 +43,9 @@ func init() {
 }
 
 func main() {
+	// Generate random seed for rng
+	rand.Seed(time.Now().UTC().UnixNano())
+
 	// Create a new Discord sessions using the provided bot token
 	dg, err := discordgo.New("Bot " + Token)
 	if err != nil {
@@ -64,10 +83,11 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) { // Messag
 	// The message, split up
 	splitMessage := strings.Split(m.Content, " ")
 
-	msgHandler := NewMessageHandler(splitMessage[0], splitMessage, commands) // splitMessage[0] is the command itself
+	msgHandler := MessageHandler{splitMessage[0], splitMessage, commands} // splitMessage[0] is the command itself
 
 	// Handles basic messages
 	msgHandler.BasicMessages(s, m)
 
+	// Handles advanced messages
+	msgHandler.AdvancedMessages(s, m, gophers)
 }
-
--- messageHandler.go
+++ messageHandler.go
@@ -4,17 +4,14 @@ import (
 	"github.com/bwmarrin/discordgo"
 
 	"fmt"
+	"math/rand"
 	"strings"
 )
 
-type MessageHandler struct {
+type MessageHandler struct { // This deals with all message inputs from the user.
 	command string // Command user typed
 	messageSlice []string
 	commands []string // Commands: *Hello, *Help, *Gopherify
-}
-
-func NewMessageHandler(cmd string, messages []string, cmds []string) *MessageHandler { // 'Constructor' for MessageHandler
-	return &MessageHandler{cmd, messages, cmds}
 }
 
 func (mH MessageHandler) BasicMessages(s *discordgo.Session, m *discordgo.MessageCreate) { // Deals with the basic commands
@@ -22,10 +19,21 @@ func (mH MessageHandler) BasicMessages(s *discordgo.Session, m *discordgo.Messag
 		s.ChannelMessageSend(m.ChannelID, "`Henlo, I am Gopher Bot`")
 	}
 	if mH.command == mH.commands[1] { // Help
-		help := fmt.Sprintf("`Commands:\n%s\n%s\n%s <YOUR MESSAGE>`", mH.commands[0], mH.commands[1], mH.commands[2])
+		help := fmt.Sprintf("`Commands:\n%s\n%s\n%s <YOUR MESSAGE>\n%s`", mH.commands[0], mH.commands[1], mH.commands[2], mH.commands[3])
 		s.ChannelMessageSend(m.ChannelID, help)
 	}
 	if mH.command == mH.commands[2] { // Gopherify
 		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("`%s`", strings.Repeat("Squeak ", len(mH.messageSlice)-1))) // -1 because the command is included.
 	}
+}
+
+func (mH MessageHandler) AdvancedMessages(s *discordgo.Session, m *discordgo.MessageCreate, gophers []string) {
+	if(mH.command == mH.commands[3]) { // Gopher
+		randGopher := randInt(0, len(gophers))
+		s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{Title: "Gopher:", Image: &discordgo.MessageEmbedImage{URL:gophers[randGopher]}})
+	}
+}
+
+func randInt(min int, max int) int {
+	return min + rand.Intn(max-min)
 }
